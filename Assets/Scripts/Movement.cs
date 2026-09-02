f263c211a12244224310179c9481c6e1533261ee
Lionel170401 <77852878+Lionel170401@users.noreply.github.com>
2026-09-02 16:03:51 -0300

    Mejora del movimiento
    Ahora el tiempo de salto es de 1s
    El personaje puede moverse menos en el aire que en el suelo

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,30 +4,35 @@ public class Movement : MonoBehaviour
 {
     private Rigidbody rb;
     private PlayerInput playerInput;
+    //esta variable nos va a ayudar a controlar el alto del salto y la cantidad de tiempo en el aire
     [SerializeField] private float jumpForce;
+    // esta variable va a guardar la direccion del movimiento al momento de apretar el boton de salto para guardarlo
+    // y poder conservar esa direccion
+    private Vector3 jumpDirection;
+    // esta variable la vamos a usar para determinar cuanto se puede mover el personaje en el aire
+    [SerializeField] private float jumpMovement;
+    // esta va a ser la velocidad maxima
     [SerializeField] private float maxSpeed;
+    // esta variable la vamos a usar para que el personaje se mueva mas rapido al moverse
     [SerializeField] private float acceleration;
+    // esta variable la vamos a usar para que el personaje se detenga mas rapido, sin tanta inercia
     [SerializeField] private float deceleration;
-    [SerializeField] private float gravityFactor;
     private Vector3 movementDirection;
     private bool isGrounded;
 
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         playerInput = GetComponent<PlayerInput>();
     }
-
-    // Update is called once per frame
     void Update()
     {
         // leemos el input del teclado y lo guardamos en un vector3
         movementDirection = playerInput.actions["Movement"].ReadValue<Vector3>();
         movementDirection.Normalize();
         GroundCheck();
-        Debug.Log("La variable está en el piso es " + isGrounded);
+        //Debug.Log("La variable está en el piso es " + isGrounded);
         // Normalizo el vector de entrada
         // ya que despues lo voy a multiplicar por la velocidad
         // de otra manera podria hacer que yendo en diagonal sea mas rapido 
@@ -58,16 +63,17 @@ public class Movement : MonoBehaviour
         Vector3 newVelocity = Vector3.MoveTowards(actualVelocity, targetVelocity, velocityChange * Time.fixedDeltaTime);
 
         //ahora hay que cambiarle la velocidad al rigidbody en linea con la del vector newVelocity
-        rb.linearVelocity = new Vector3(newVelocity.x, actualVelocity.y, newVelocity.z);
-
-        if (rb.linearVelocity.y < -1f)
+        // este if nos permite controlar diferentes movimientos dependiendo si el personaje esta en el suelo o saltando
+        if (isGrounded)
         {
-            rb.linearVelocity = new Vector3(rb.linearVelocity.x, rb.linearVelocity.y * gravityFactor, rb.linearVelocity.z);
+            rb.linearVelocity = new Vector3(newVelocity.x, actualVelocity.y, newVelocity.z);
         }
-        else if (rb.linearVelocity.y > 6f)
+        else
         {
-
+            jumpDirection = Vector3.MoveTowards(jumpDirection,targetVelocity,jumpMovement * Time.fixedDeltaTime);
+            rb.linearVelocity = new Vector3(jumpDirection.x, rb.linearVelocity.y, jumpDirection.z);
         }
+
     }
 
     public void Jump(InputAction.CallbackContext callbackContext)
@@ -75,16 +81,21 @@ public class Movement : MonoBehaviour
 
         if (callbackContext.performed && isGrounded)
         {
-            rb.AddForce(0, jumpForce, 0, ForceMode.Impulse);
-            //rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, rb.linearVelocity.z);
+            //aca guardo la direccion del salto
+            jumpDirection = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
+            // aca le pongo velocidad en y para que salte
+            rb.linearVelocity = new Vector3(jumpDirection.x, jumpForce, jumpDirection.z);
         }
 
     }
     private void GroundCheck()
     {
+        //esta es una forma "sencilla" de determinar si el personaje esta en el piso y puede saltar o no
+        // el raycast tira un rayo para abajo 
+        // si detecta un objeto Physics.Raycast nos devuelve un booleano con valor true y si no detecta nada nos devuelve false
         Ray ray = new Ray(transform.position, Vector3.down);
-        Debug.DrawRay(ray.origin, ray.direction * 1.5f, Color.green);
-        if (Physics.Raycast(ray, 1.5f))
+        Debug.DrawRay(ray.origin, ray.direction * 1f, Color.green);
+        if (Physics.Raycast(ray, 1f))
         {
             isGrounded = true;
         }
